83589f7c48077eaf3bae577b96c22b2d08b4082a
John McElreavey <jmcelreavey@gcdtech.com>
2024-01-02 20:56:57 +0000

    Remove unused import and variable in Providers component

--- apps/www/src/components/providers.tsx
+++ apps/www/src/components/providers.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 export function Providers({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-
   return <>{children}</>;
 }
