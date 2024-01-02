e3eabf16aa4eab405a388af05497a703bd6a4376
John McElreavey <jmcelreavey@gcdtech.com>
2024-01-02 21:07:13 +0000

    Update next.config.js and package.json

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,31 +1,29 @@
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 /** @type {import('next').NextConfig} */
 
-export const reactStrictMode = true;
-export const transpilePackages = [
-  "@fleabay/db",
-  "fleabayto/auth",
-  "@fleabay/api",
-];
-export const experimental = {
-  webpackBuildWorker: true,
+module.exports = {
+  reactStrictMode: true,
+  transpilePackages: ["@fleabay/db", "fleabayto/auth", "@fleabay/api"],
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+    return config;
+  },
 };
-export const images = {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "lh3.googleusercontent.com",
-    },
-    {
-      protocol: "https",
-      hostname: "utfs.io",
-    },
-  ],
-};
-export function webpack(config, { isServer }) {
-  if (!isServer) {
-    config.plugins = [...config.plugins, new PrismaPlugin()];
-  }
-  return config;
-}
